e2778f507b4f63aa6cc3db9d2efd06fe552b42b4
2200031907 <147921756+2200031907@users.noreply.github.com>
2023-10-14 17:16:36 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,15 @@
+<html lag="en">
+    <head>
+        <title> nav bar</title>
+        <style>
+            
+        </style>
+    </head>
+
+    <body>
+        <header><h2> NAVIGATION BAR</h2></header>
+        <nav>
+           <a href="#">home</a>
+        </nav>
+    </body>
+</html>
